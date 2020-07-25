6b7de15b9c3c8ea678d9f51dcf2295e3c1259889
A Findlay <skylinerspeeder@gmail.com>
2020-07-25 03:29:54 +0000

    Structure fetched pricing data

--- apps/esi/dataclasses.py
+++ apps/esi/dataclasses.py
@@ -3,7 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class MarketOrder:
-    type_id: int
-    order_type: str
     price: float
     volume_remain: int
--- apps/esi/tasks.py
+++ apps/esi/tasks.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import List
+from typing import Dict, List
 
 from jita.celery import app
 from apps.sde.models import Region
@@ -16,15 +16,15 @@ def update_region_prices(region_id: int):
 
     # Fetch all pricing data from a region
     print(f"Fetching prices for {region.name}")
-    orders: list[MarketOrder] = []
+    orders: Dict[int, Dict[str, List[MarketOrder]]] = (
+        defaultdict(lambda: defaultdict(list)))
 
     i = 1
     while True:
         response = get_region_orders(region_id, page=i).json()
         for order in response:
-            orders.append(MarketOrder(
-                type_id=order['type_id'],
-                order_type=OrderType.BUY if order['is_buy_order'] else OrderType.SELL,
+            order_type = OrderType.BUY if order['is_buy_order'] else OrderType.SELL
+            orders[order['type_id']][order_type].append(MarketOrder(
                 price=order['price'],
                 volume_remain=order['volume_remain']))
 
@@ -35,4 +35,4 @@ def update_region_prices(region_id: int):
             break
         i += 1
 
-    print(orders[0])
+    
