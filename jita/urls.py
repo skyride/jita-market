3f2e20813cc47b8ac97f9d856afd0e09331340dc
A Findlay <skylinerspeeder@gmail.com>
2020-07-25 22:28:55 +0000

    Put swagger on root

--- jita/urls.py
+++ jita/urls.py
@@ -20,7 +20,7 @@ router.register(r"v1/types", api_views.TypeV1ViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls)
 ]
